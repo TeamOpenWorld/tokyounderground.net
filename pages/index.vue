0abbe5a2b723edb1f954332d5a6a6b84116b2a41
Minato86 <doronomori660@outlook.jp>
2020-08-02 21:45:18 +0900

    indexがバグってた

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,5 @@
 <template>
     <div>
-<<<<<<< Updated upstream
-        <v-parallax
-            dark
-            src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-        >
-            <v-row align="center" justify="center">
-                <v-col class="text-center" cols="12">
-                    <h1 class="display-1 font-weight-thin mb-4">
-                        {{ $t("index.top") }}
-                    </h1>
-                    <h4 class="subheading">{{$t("index.description")}}</h4>
-                </v-col>
-            </v-row>
-        </v-parallax>
-        <v-container class="py-16">
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-            <h1>ジョウタだよ～～～</h1>
-        </v-container>
-=======
->>>>>>> Stashed changes
     </div>
 </template>
 

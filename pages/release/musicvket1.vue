a1b53d9ba813822347a5a0ce5b59838162d98250
Minato86 <doronomori660@outlook.jp>
2020-08-08 04:35:30 +0900

    トップページ実装

--- pages/release/musicvket1.vue
+++ pages/release/musicvket1.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
-        <p>aaa</p>
+        <div class="w-full h-screen text-white bg-gray-900 overflow-hidden">
+            <img
+                src="~/assets/initialized_logo.svg"
+                class="h-full w-full transform -translate-x-96 fill-current text-blue-200 absolute"
+            />
+            <div class="flex content-center flex-wrap h-screen transform translate-x-84">
+                <div class="w-full text-center ">
+                    <p class="text-2xl">始めよう、最初から。</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <p>hello</p>
+        </div>
     </div>
 </template>
+
+<script>
+    export default {
+        asyncData() {},
+
+        head() {
+            return {
+                title: "MusicVket1",
+            };
+        },
+    };
+</script>

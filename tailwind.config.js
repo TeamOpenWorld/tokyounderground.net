7c373113b1ffabdccf4e6bfcf9bb6182521cd3c6
Minato86 <doronomori660@outlook.jp>
2020-08-09 14:35:10 +0900

    アスペクト比のやつを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
                 "96": "24rem",
             },
         },
+        aspectRatio: {
+            none: 0,
+            square: [1, 1],
+            "16/9": [16, 9],
+            "4/3": [4, 3],
+            "21/9": [21, 9],
+        },
     },
-    variants: {},
-    plugins: [],
+    variants: { aspectRatio: ["responsive"] },
+    plugins: [require("tailwindcss-aspect-ratio")],
 };

bae0dd1884d83e1ca0aca1a9d21b9ae4df63dff0
Minato Minami <doronomori660@outlook.jp>
2021-03-02 22:04:21 +0900

    add: musicvket2のtitle設定

--- pages/release/musicvket2.vue
+++ pages/release/musicvket2.vue
@@ -1,3 +1,17 @@
 <template>
     <p>工事中・・・</p>
 </template>
+
+<script>
+export default {
+    layout: "main",
+
+    asyncData() {},
+
+    head() {
+        return {
+            title: "MusicVket2",
+        };
+    },
+};
+</script>

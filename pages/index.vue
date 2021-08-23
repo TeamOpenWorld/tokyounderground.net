2fa71e99a0ce67cab569e317d6b754abdf816d24
Minato Minami <doronomori660@outlook.jp>
2021-08-24 05:01:53 +0900

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,13 @@
 <template>
-    <div id="bg" class="flex content-center flex-wrap h-screen">
-        <div class="w-full text-center">
-            <p>Hello, We are TokyoUnderground.</p>
-            <p>TokyoUnderground is a music promotion network from Japan.</p>
-            <br />
-            <p>TokyoUndergroundのウェブサイトへようこそ！</p>
-            <p>
-                私達は音楽文化をもっと盛り上げるべく活動している、日本の楽曲プロモーションネットワークです。
-            </p>
-            <br />
-            <NuxtLink class="link" to="/release/musicvket1"
-                >MusicVket1 (Japanese page Only)</NuxtLink
-            ><br />
-            <a href="./release/musicvket2">MusicVket2 (Japanese page Only)</a>
-            <br />
-            <a href="https://open-w.net">運営情報 / Organization</a>
-        </div>
+    <div>
+        <section id="top" class=" h-screen flex justify-start items-center">
+            <div class="ml-48">
+                <p class=" text-4xl">Welcome to Tokyo Underground</p>
+                <p class=" mt-4 text-xl">個人クリエイターのための音楽コミュニティ</p>
+            </div>
+        </section>
     </div>
 </template>
-
-<style scoped>
-#bg {
-    background-color: rgb(31, 31, 31);
-}
-
-p {
-    color: white;
-}
-
-.link {
-    text-decoration: underline;
-    color: white;
-}
-
-a {
-    text-decoration: underline;
-    color: white;
-}
-</style>
 
 <script>
 export default {
